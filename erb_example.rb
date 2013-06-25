e00ea90c15cfc43e571701f49e987966d49d027c
MakerSquare Student <student@lab-7.att.net>
2013-06-25 15:29:09 -0500

    step 3, using ERB

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,17 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+product = x*2
+template = ERB.new "Twice the value of x is: <%= product %>"
+puts template.result(binding)
+
+drinks = %w{Water Scotch Cocoa}
+number = 1
+drinks.each do |drink|
+
+template = ERB.new "Drink <%= number %>: <%= drink %>"
+puts template.result(binding)
+
+number = number + 1
+end
